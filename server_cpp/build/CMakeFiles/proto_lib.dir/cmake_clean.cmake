411b6eeef81c58bc22563f41aec701ffe99fabea
Danilo Honda <hondadanilo6@gmail.com>
2025-10-24 01:43:39 +0000

    add project architecture

---
+++ server_cpp/build/CMakeFiles/proto_lib.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/proto_lib.dir/file_processor.grpc.pb.cc.o"
+  "CMakeFiles/proto_lib.dir/file_processor.grpc.pb.cc.o.d"
+  "CMakeFiles/proto_lib.dir/file_processor.pb.cc.o"
+  "CMakeFiles/proto_lib.dir/file_processor.pb.cc.o.d"
+  "file_processor.grpc.pb.cc"
+  "file_processor.grpc.pb.h"
+  "file_processor.pb.cc"
+  "file_processor.pb.h"
+  "libproto_lib.a"
+  "libproto_lib.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/proto_lib.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
